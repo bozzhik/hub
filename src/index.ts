951c803e2057bab32396269d2d8b71af283a1df0
maxim <bozzhik@gmail.com>
2026-01-18 15:55:33 +0300

    add `projects` data for `main` page

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
+import {projects} from '@/data'
 import {createLayout} from '@/layout'
 
 import {Elysia} from 'elysia'
 import {html} from '@elysiajs/html'
 
 export default new Elysia().use(html()).get('/', ({html}) => {
+  const projectsHtml = projects
+    .map(
+      (project) => `
+      <article>
+        <h3>
+          ${project.status === 'prod' ? `<a href="https://${project.name}.wzx.cx">${project.name}</a>` : `${project.name} <small>🛠️</small>`}
+        </h3>
+        <p>${project.description}</p>
+      </article>
+    `,
+    )
+    .join('')
+
   return html(
     createLayout(
       'hub',
@@ -12,6 +26,10 @@ export default new Elysia().use(html()).get('/', ({html}) => {
           <h1>bozzhik's hub</h1>
           <p>some projects under <strong>wzx.cx</strong></p>
         </header>
+
+        <section>
+          ${projectsHtml}
+        </section>
 
         <footer>
           <p><em>visit <a href="https://bozzhik.com">bozzhik.com</a> to reach me</em></p>
